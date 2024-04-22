90a15362a5cbed9c13e320d17d26e0f82e11470b
abbylockhart <abbyllockhart@gmail.com>
2024-04-22 14:58:11 +1000

    removed unneccessary code

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
 console.log("hello " + "world");
-
-function codingBoss() {
-    // This is an alert for when the condingBoss function is called
-    // alert("hey you!");
-    document.querySelector('h1').style.color = "hotpink";
-}
-codingBoss();
