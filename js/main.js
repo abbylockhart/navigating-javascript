5b8f42aac0f26df9280c4246b7c4ec26d4ad1cbb
abbylockhart <abbyllockhart@gmail.com>
2024-04-22 14:51:02 +1000

    first initial push live

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log("hello " + "world");
+
+function codingBoss() {
+    // This is an alert for when the condingBoss function is called
+    // alert("hey you!");
+    document.querySelector('h1').style.color = "hotpink";
+}
+codingBoss();
